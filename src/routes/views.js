94baff46f5446e5e8dbb2f19be9628ca35dc1971
Sébastien Lampazona <sebastien@oclock.io>
2022-10-11 14:58:17 +0200

    Qui qui avait pas tout save ?

--- src/routes/views.js
+++ src/routes/views.js
@@ -3,6 +3,7 @@ import * as controllers from 'src/controllers';
 export default (app) => {
   app.view('new_participation', controllers.newParticipation);
   app.view('send_message_secret_santa', controllers.sendMessageSecretSanta);
+  app.view('send_message_victim', controllers.sendMessageVictim);
 
   return app;
 };
